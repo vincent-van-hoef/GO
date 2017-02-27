90e7afadbfbd5623b1ab0deac3a8f3146a8cf30e
vincent-van-hoef <vvanhoef@hotmail.com>
2017-02-27 14:50:11 +0100

    Create helpers.R
    Set of helper functions for the analysis of GO term data

---
+++ helpers.R
@@ -0,0 +1,34 @@
+# Collect all genes related to certain cellular function in GO.db, this uses a regular expression to find GO terms. GO.db has to be laoded! This is species independent
+grepGo <- function(term){
+  goTerms <- unlist(Term(GOTERM))
+  grepTerms <- grep(term, goTerms)
+  goTerms[grepTerms]
+} 
+
+# Use output of grepGO to collect all genes related to certain grepped term or other GO ID list named with the GO ID. Returns the unique genes attached to the terms, not to the children terms.
+collectGenesGrepGo_mouse <- function(grepGo, ...){
+  select(org.Mm.eg.db, keys = names(grepGo), keytype = "GO", columns = c("GO", "SYMBOL", "ENTREZID"))
+}
+
+collectGenesGrepGo_human <- function(grepGo, ...){
+  select(org.Hs.eg.db, keys = names(grepGo), keytype = "GO", columns = c("GO", "SYMBOL", "ENTREZID"))
+}
+
+# Combine the grep of GO terms and collection of genes in a single function. Return a vector of unique gene symbols or entrez ids related to the GO term.
+collectSymbolGoTerm_mouse <- function(term){
+  tmpGrepGo <- grepGo(term)
+  tmpGenes <- collectGenesGrepGo_mouse(tmpGrepGo)
+  unique(as.vector(tmpGenes[,"SYMBOL"]))
+}
+
+collectEntrezGoTerm_mouse <- function(term){
+  tmpGrepGo <- grepGo(term)
+  tmpGenes <- collectGenesGrepGo_mouse(tmpGrepGo)
+  unique(as.vector(tmpGenes[,"SYMBOL"]))
+}
+
+# Map entrez to symbol, this can return multiple gene symbols per entrez id but this function only returns the unique names!
+convertEntrez <- function(x){
+  tmp <- select(org.Mm.eg.db, keys = x, keytype = "ENTREZID", columns =c("ENTREZID", "SYMBOL"))
+  unique(as.vector(tmp[,"SYMBOL"]))
+}
